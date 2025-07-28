f8e157b8ca922ef4578ca5f10076b89226f68998
imlyzh <enterindex@gmail.com>
2025-07-28 16:55:10 +0800

    Disable snacks animation and enable spellcheck
    - Comment out termguicolors setting as it may not be needed
    - Disable snacks plugin animation via vim.g.snacks_animate
    - Enable spellcheck with CJK language support for better text editing

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.opt.termguicolors = true
+-- vim.opt.termguicolors = true
+vim.g.snacks_animate = false
+vim.opt.spell = true
+vim.opt.spelllang = { "cjk" }
